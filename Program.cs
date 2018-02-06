dc0cdc67539868748ccd789c0e56327c8b53067d
DESKTOP-34JNQ2S\smori <mori1979shingo@gmail.com>
2018-02-06 17:46:34 +0900

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace DeleteDocumentFromCosmosDB
+{
+    class DocumentDbManager
+    {
+        // DocumentDbManagerに定数定義を追加
+        private const string EndpointUrl = "自分のDBのエンドポイント";
+        private const string PrimaryKey = "自分のDBのキー";
+        private const string DatabaseId = "outDatabase";    //自身のDB名に変更
+        private const string CollectionId = "MyCollection"; //自身のコレクションIDに変更
+
+        private DocumentClient client = null;
+
+        public DocumentDbManager()
+        {
+            this.client =
+              new DocumentClient(
+                new Uri(DocumentDbManager.EndpointUrl),
+                DocumentDbManager.PrimaryKey);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main よばれたよ");
+
+            var manager = new DocumentDbManager();
+
+
+            List<Record> records = manager.FindAllRecords();
+
+            // 検索結果出力
+            Console.WriteLine(string.Format("{0}件", records.Count));
+            foreach (var info in records)
+            {
+                Console.WriteLine(
+                  string.Format("id = {0} delete. ", info.Id));
+
+                manager.DeleteById(info.Id).Wait();
+            }
+
+        }
+
+        // DatabaseId、CollectionIdで全件取得するメソッド
+        public List<Record> FindAllRecords()
+        {
+            var query =
+              this.client.CreateDocumentQuery<Record>(
+                UriFactory.CreateDocumentCollectionUri(
+                  DocumentDbManager.DatabaseId, DocumentDbManager.CollectionId));
+            var result = query.ToList();
+
+            return result;
+        }
+
+        // id指定で1件削除するメソッド
+        public async Task<Document> DeleteById(string id)
+        {
+            var document = await this.client.DeleteDocumentAsync(
+              UriFactory.CreateDocumentUri(
+                DocumentDbManager.DatabaseId,
+                DocumentDbManager.CollectionId,
+                id));
+
+            return document;
+        }
+    }
+
+    class Record
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# CosmosDBの任意のドキュメントからレコードを全件削除するためのプログラム
+# VisualStudio側でMicrosoft.Azure.Document.Coreが必要（NuGetでインストール）
+
+参考サイト [Azure Cosmos DB入門（3）](http://ryuichi111std.hatenablog.com/entry/2017/05/28/145047#322-%E3%83%87%E3%83%BC%E3%82%BF%E3%83%99%E3%83%BC%E3%82%B9%E3%81%A8%E3%82%B3%E3%83%AC%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AE%E4%BD%9C%E6%88%90)
